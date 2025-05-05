647c3d7316ba9490d1d39c00c69f756fc583e2d4
bushman <bushman@tecno.local>
2025-05-05 20:26:54 +0300

    added target's json file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+
+pub mod vga;
+
+use core::panic::PanicInfo;
+
+
+#[panic_handler]
+fn panic_handler(_info:&PanicInfo)->!{
+    loop{}
 }
+#[no_mangle]
+pub extern "C" fn _start()->!{
+    loop{}
+}
+
