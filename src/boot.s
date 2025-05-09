d16fad397c74b4c9c38c7f8bf749d802a3d14bf5
bushman <bushman@tecno.local>
2025-05-09 17:53:57 +0300

    boot.s: Access multiboot info structure.        GRUB passess Memory or  in general Multiboot Info Structure        through a register EBX.        To ensure that the info is correct,a Multiboot Magic Number is provided        which we should compare to the value GRUB provides for us through register        EAX.

--- src/boot.s
+++ src/boot.s
@@ -76,7 +76,10 @@ _start:
 	runtime support to work as well.
 	*/
 
-    push %ebx
+    ;Push the Multiboot info pointer (EBX) and magic number (EAX) to the stack
+    push %ebx              ; Multiboot info struct pointer
+    push %eax              ; Multiboot magic number
+
 	/*
 	Enter the high-level kernel. The ABI requires the stack is 16-byte
 	aligned at the time of the call instruction (which afterwards pushes
