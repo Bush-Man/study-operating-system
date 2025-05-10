e67a041a3d9da3c5d2c84bfe4add4e780899c3ab
bushman <bushman@tecno.local>
2025-05-10 07:14:17 +0300

    memory: Created MultibootInfoStructure.         Structure representing data from the bootloader through the         register ebx.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@
 #![no_main]
 
 mod vga;
-
+mod memory;
 use core::panic::PanicInfo;
 
+use memory::multiboot_structure::MultibootInfoStructure;
 use vga::writter::VgaWriter;
 use core::fmt::Write;
 
@@ -20,19 +21,22 @@ fn panic_handler(_info:&PanicInfo)->!{
 
 
 #[no_mangle]
-pub extern "C" fn kernel_main(multiboot_magic: u32, multiboot_info_ptr: u32)->!{
+pub extern "C" fn kernel_main(multiboot_magic: u32, multiboot_info_ptr: &MultibootInfoStructure)->!{
 
 
     if MULTIBOOT_MAGIC_NUMBER != multiboot_magic{
         panic!("Invalid Multiboot Magic Number");
     }
+
     let mut vga_writer = VgaWriter::new();
+    
 
 
     
     // write!(vga_writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
     // write!(vga_writer,"{}", UPPERCASE).unwrap();
-    write!(vga_writer,"{:#x} Magic Number",multiboot_magic).unwrap();
+    write!(vga_writer,"{:#x} Magic Number\n",multiboot_magic).unwrap();
+    write!(vga_writer,"{:?} Multiboot Info Address\n",multiboot_info_ptr).unwrap();
 
     loop{}
 }
